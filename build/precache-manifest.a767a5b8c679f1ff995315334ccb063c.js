c580b2725d6620d499af65ed8c11e6cac5fe7c3e
zackbeckwith <z.beckwith99@gmail.com>
2022-04-21 21:12:25 -0400

    fix main page links

---
+++ build/precache-manifest.a767a5b8c679f1ff995315334ccb063c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ecbb8e7e364e637e8cb48a2a6eeee0f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cd1cd96f5922bb37880e",
+    "url": "/portfolio/static/css/2.45c371c4.chunk.css"
+  },
+  {
+    "revision": "3ed8ae773ee5624466b2",
+    "url": "/portfolio/static/css/main.e8901382.chunk.css"
+  },
+  {
+    "revision": "cd1cd96f5922bb37880e",
+    "url": "/portfolio/static/js/2.8f0fa077.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.8f0fa077.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ed8ae773ee5624466b2",
+    "url": "/portfolio/static/js/main.9daf5c5e.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
