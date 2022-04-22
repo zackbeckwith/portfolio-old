f3ccbd14e23a0fd7acab4be8e07bbb08afd418d9
zackbeckwith <z.beckwith99@gmail.com>
2022-04-21 21:49:47 -0400

    add build

---
+++ build/precache-manifest.98769a6a1039b93dba10b9a5ff529f26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd50b20e2f628c06392c073bcf20281b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4c7c7418e0a00bd87433",
+    "url": "/portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "cfd7b3010c8850ae808c",
+    "url": "/portfolio/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "4c7c7418e0a00bd87433",
+    "url": "/portfolio/static/js/2.aaf5e80a.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.aaf5e80a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfd7b3010c8850ae808c",
+    "url": "/portfolio/static/js/main.7f58d576.chunk.js"
+  },
+  {
+    "revision": "6c972d9418aa5b4e59ab",
+    "url": "/portfolio/static/js/runtime-main.1b893ad1.js"
+  }
+]);
